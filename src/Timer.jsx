ee49d2b436f5b8e682135ffa5515574d26ba89ec
V1rs999 <romandraguca@gmail.com>
2024-09-16 01:01:25 +0300

    test

--- src/Timer.jsx
+++ src/Timer.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from 'react';
+import  { useState, useEffect, useRef } from 'react';
 
 const Timer = () => {
 	const [time, setTime] = useState(0);
--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
 	plugins: [react()],
-	server: {
-		open: "/",
-	},
-	preview: {
-		open: true,
-	},
+	base: "./"
 });
