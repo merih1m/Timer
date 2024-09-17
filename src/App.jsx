644fd7dd56c41bfb3292807d63cf5d4e5527cf3c
V1rs999 <romandraguca@gmail.com>
2024-09-18 00:11:12 +0300

    fix high

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import Timer from './Timer';
 
 function App() {
 	return (
-		<div className="flex justify-center  w-svw bg-gray-900 ">
+		<div className="flex justify-center  w-svw h-svw bg-gray-900 ">
 			<Timer />
 		</div>
 	);
